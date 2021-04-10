4158bbab81adfd0d6a1f3a3c4821b56ddff54224
Stefan Nedelcu <stefan.n@gmx.co.uk>
2021-04-10 22:41:56 +0100

    commit on 'main'

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 $(document).ready(function(){
-	let color_theme = "light";
+	if (localStorage.getItem("color") === "dark"){
+		$(".change").addClass("toDark");	
+		$("body").css("background-color", "gray");
+		$("textarea").css("background-color", "lightgray");
+	}
 	$(".change_theme").click(function(){
 		$(".change").toggleClass("toDark");
 		if ($(".change").hasClass("toDark")){
 			$("body").css("background-color", "gray");
 			$("textarea").css("background-color", "lightgray");
+			localStorage.setItem("color", "dark");
 		}
 		else {
 			$("body").css("background-color", "white");	
 			$("textarea").css("background-color", "white");
+			localStorage.setItem("color", "light");
 		}
 	})
 })
